676253b41a83663178b87bbf389ad913a370f495
MutaharPathan <161881871+mutahar-pathan@users.noreply.github.com>
2026-02-03 20:41:27 +0530

    add register api

--- backend/app/auth/user_auth.py
+++ backend/app/auth/user_auth.py
@@ -1,10 +1,21 @@
-from app.models.user_auth import Register
+from app.models.user_auth import Register , Login
 from app.database.db import auth_user
+from fastapi import HTTPException , APIRouter
 
+auth_router = APIRouter()
+
+@auth_router.post("/register")
 def register_user(register : Register):
+    if auth_user.find_one({"email" : register.email}):
+        raise HTTPException(
+            status_code = 400,
+            detail = "Email already registered"
+        )
     auth_user.insert_one(register.dict())
-    return{"message : register successfully"}
 
-
-
+    return{
+        "message : register successfully"
+    }
  
+ def login_user(login : Login):
+    auth_user.find
