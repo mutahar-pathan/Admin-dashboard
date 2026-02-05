7cca9d85c8acd59847c53a2f6d2e634b610441fa
MutaharPathan <161881871+mutahar-pathan@users.noreply.github.com>
2026-02-05 11:57:35 +0530

    added authetication

---
+++ backend/app/auth/roles.py
@@ -0,0 +1,39 @@
+from fastapi import Depends, HTTPException, status
+from bson import ObjectId
+from app.auth.token import verify_token
+from app.database.db import auth_user
+from app.utils.serializer import serialize_user
+
+
+def get_current_user(payload=Depends(verify_token)):
+    user_id = payload.get("user_id")
+
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    user = auth_user.find_one(
+        {"_id": ObjectId(user_id)}
+    )
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return serialize_user(user) 
+
+
+def admin_only(current_user=Depends(get_current_user)):
+    if current_user.get("role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access only"
+        )
+    return current_user
+
+
+def user_only(current_user=Depends(get_current_user)):
+    if current_user.get("role") not in ["user", "admin"]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User access only"
+        )
+    return current_user
